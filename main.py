d00558fc1985a9699b80c13d83c8b75d1f2c9d4d
WM <will@loblollysoftware.com>
2020-03-30 14:23:49 -0500

    to threading!

--- main.py
+++ main.py
@@ -1,71 +1,33 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-import curses
 import sys
 import os
-from curses.textpad import Textbox, rectangle
+import curses
+import threading
+import time
+from player.curses_object import Curse
 
 
 class Main:
 
     def __init__(self):
+        self.menu_items = ['Log In']
+        self.curse_window = None
+        self.curse_thread = None
         curses.wrapper(self.start_curses)
 
     def start_curses(self, stdscr):
-        curses.noecho()
-
-        # Clear and refresh the screen for a blank canvas
-        stdscr.clear()
-
-        stdscr.refresh()
-
-        # Start colors in curses
-        curses.start_color()
-        # these are for UI
-        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_CYAN)
-
-        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_BLACK)
-        curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_GREEN)
-        curses.init_pair(7, curses.COLOR_CYAN, curses.COLOR_WHITE)
-        curses.init_pair(8, curses.COLOR_RED, curses.COLOR_BLUE)
-
-        self.run(stdscr)
-
-    def run(self, stdscr):
-        k = 0
-        cursor_y = 0
-
-        height, width = stdscr.getmaxyx()
-
-        # Create menu and output windows
-        outscr = curses.newwin(int(height / 2), width, int(height / 2), 0)
-        outscr.box()
-
-        menuscr = curses.newwin(int(height / 2), width, 0, 0)
-        menuscr.box()
-
-        outscr.refresh()
-        menuscr.refresh()
-
-        # Loop where k is the last character pressed
-        while k != ord('q'):
-            # Initialization
-            curses.curs_set(0)
-
-            cursor_y = max(0, cursor_y)
-            # cursor_y = min(len(self.menu) - 1, cursor_y)
-
-            # draw some borders
-#
-#            # refresh screen
-#            printscr.refresh()
-
-            # Wait for next input
-            if k != ord('q'):
-                k = stdscr.getch()
+        self.curse_window = Curse(stdscr)
+        self.curse_thread = threading.Thread(
+            name='curse_daemon',
+            target=self.curse_window.run,
+        )
+        self.curse_thread.start()
+        time.sleep(1)
+        self.curse_window.add_to_queue('aaaast')
+        self.curse_window.add_to_queue('tesst')
+        time.sleep(1)
+        self.curse_window.add_to_queue('tesst')
 
 
 if __name__ == '__main__':
---
+++ player/curses_object.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import curses
+import enum
+from collections import deque
+# from curses.textpad import Textbox, rectangle
+
+
+class Color(enum.Enum):
+    background = 1
+    text = 2
+
+
+class Curse:
+
+    def __init__(self, stdscr):
+        self.log_queue = deque()
+        self.y = 1
+        self.x = 1
+        curses.noecho()
+
+        # Clear and refresh the screen for a blank canvas
+        stdscr.clear()
+
+        stdscr.refresh()
+
+        # Start colors in curses
+        curses.start_color()
+
+        # these are for UI
+        curses.init_pair(Color.background.value, curses.COLOR_CYAN,
+                         curses.COLOR_BLACK)
+        curses.init_pair(Color.text.value, curses.COLOR_BLACK,
+                         curses.COLOR_WHITE)
+
+        self.stdscr = stdscr
+        self.height, self.width = self.stdscr.getmaxyx()
+        self.menuscr = curses.newwin(int(self.height / 2), self.width, 0, 0)
+        self.outscr = curses.newwin(int(self.height / 2), self.width,
+                                    int(self.height / 2), 0)
+
+    def add_to_queue(self, val):
+        self.log_queue.append(val)
+
+    def print_log(self):
+        if self.log_queue:
+            text = self.log_queue.popleft()
+            self.menuscr.addstr(self.y, self.x, text)
+            self.y += 1
+            self.menuscr.refresh()
+
+    def run(self):
+        k = 0
+        # cursor_y = 0
+
+        # Create menu and out/put windows
+        self.outscr.box()
+
+        self.menuscr.box()
+
+        self.outscr.refresh()
+        self.menuscr.refresh()
+
+        # Loop where k is the last character pressed
+        while k != ord('q'):
+            self.print_log()
+            # Initialization
+            # curses.curs_set(0)
+
+            # cursor_y = max(0, cursor_y)
+            # self.print_log()
+            # cursor_y = min(len(self.menu) - 1, cursor_y)
+
+            # draw some borders
+            #
+            #            # refresh screen
+            #            printscr.refresh()
+
+            # Wait for next input
+            if k != ord('q'):
+                k = self.stdscr.getch()
